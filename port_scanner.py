10667ab3a573664ea8c993aeac9230b318c6062d
adlinare <caos1234matao@gmail.com>
2026-09-16 12:12:53 +0200

    Informacion extra enviando cabecera para describir servicios http

--- port_scanner.py
+++ port_scanner.py
@@ -4,8 +4,20 @@
 
 import socket
 import argparse
+import signal
+import sys
 from termcolor import colored
 from concurrent.futures import ThreadPoolExecutor #Esta clase nos permite limitar la  pool de ejecuon para no desbordar los hilos
+
+open_sockets = []
+
+def def_handler(sig, frame):
+    print(colored(f"\n[!] Saliendo del programa...", 'red'))
+    for socket in open_sockets:
+        socket.close()
+
+    sys.exit(1)
+signal.signal(signal.SIGINT, def_handler)# Ctrl + c
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Fast TCP Port Scanner')
@@ -23,6 +35,9 @@ def create_socket():
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Este es nuestro socket AF_INET ya que trabajamos con IPV4 y SOCK_STREAM por usar tcp como conexion
     s.settimeout(1) #EStablecemos el tiempo maximo de comprobacion de 1 segundo, si en un segundo no nos conectamos la conxion no es posible
+    
+    open_sockets.append(s)
+
     return s
 
 
@@ -31,10 +46,21 @@ def port_scanner(port, host):
     s = create_socket()
     #Este es otro sistema, empleando connect y viendo si devuelve error de conexion o timeout por espera exesiva 
     try:
+
         s.connect((host, port))
-        print(colored(f"El puerto {port} esta abierto", 'green'))
-        s.close()
+        #Nos quedamos con las cabeceras para saber ante que servicio nos encontramos
+        s.sendall(b"HEAD / HTTP/1.0\r\n\r\n")
+        response = s.recv(1024).decode(errors='ignore').split('\n')
+
+        if response:
+            print(colored(f"\n[+] El puerto {port} esta abierto\n",'green'))
+            for lines in response:
+                print(colored(f"{lines}", 'grey'))
+        else:
+            print(colored(f"[+]El puerto {port} esta abierto", 'green'))
     except (socket.timeout, ConnectionRefusedError):
+        pass 
+    finally:
         s.close()
 
 
